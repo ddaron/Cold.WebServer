83d5ef4a7bb3daadb63a248726c4fc2dd8fe7c57
Jakub Zimny <zimny.jot@gmail.com>
2017-07-17 23:27:02 +0200

    add tests

---
+++ Cold.WebServer.Tests/RequestFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using Xunit;
+
+namespace Cold.WebServer.Tests
+{
+    public class RequestFactoryTests
+    {
+        [Fact]
+        public void RequestFactory_GETHTTP1RequestBuffer_ReturnsResponseOKStatus()
+        {
+        //Arrange
+            var factory = new RequestFactory();
+            var simpleRequest = Encoding.ASCII.GetBytes("GET / HTTP1.0");
+            
+            //Act
+            var response = factory.Create(simpleRequest);
+            
+            //Assert
+//            response.Sta.Should().Be(HttpStatusCode.Ok);
+
+        }
+        
+    }
+}
